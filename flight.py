da7b7109874335a65ed6708be08765dd5de5d436
mezue30000 <mezue3000>
2025-02-25 04:05:43 +0100

    created flight model

--- flight.py
+++ flight.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 from sqlmodel import SQLModel, Relationship, Field, CheckConstraint, func
 from typing import Optional, List
-from datetime import date, datetime, timezone
+from datetime import date, datetime, time
 
 
 # create passenger model
@@ -13,4 +13,17 @@ class Passenger(SQLModel, table=True):
     email: str = Field(..., max_length=75, unique=True)
     birth_date: date = Field(...)
     phone_number: str = Field(..., max_length=18)
+    
+
+class Flight(SQLModel, table=True):
+    fligth_id: Optional[int] = Field(default=None, primary_key=True)
+    flight_number: str = Field(..., max_length=25)
+    departure_time: datetime = Field(...,)
+    arrival_time: datetime = Field(...)
+    distance_in_miles: int = Field(...)
+    fligth_duration: time = Field(...)
+    destination: str = Field(..., max_length=25)
+
+
+# create a link model
     
